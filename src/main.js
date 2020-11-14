a64097a7fb33d1cb43d781ed1646a491e9737f15
Jack Fletcher <jackowenfletcher@gmail.com>
2020-11-14 15:07:22 +0000

    Add PWA manifest and service worker

--- src/main.js
+++ src/main.js
@@ -23,6 +23,8 @@ import '@ionic/vue/css/display.css';
 /* Theme variables */
 import './theme/variables.css';
 
+import './registerServiceWorker'
+
 const app = createApp(App)
   .use(IonicVue)
   .use(router);
