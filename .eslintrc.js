752ed414be6cb2c39c331a75867655a12a39d758
Jack Fletcher <jackowenfletcher@gmail.com>
2020-12-06 22:31:08 +0000

    Add prettier formatter

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,36 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    node: true,
   },
-  'extends': [
+  extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
+    'prettier',
+    'prettier/vue',
+    'plugin:prettier/recommended',
   ],
+  plugins: ['prettier'],
   parserOptions: {
-    ecmaVersion: 2020
+    ecmaVersion: 2020,
+    parser: 'babel-eslint',
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'vue/no-deprecated-slot-attribute': 'off',
+    'no-unused-vars': ['warn', { vars: 'all', args: 'none' }],
+    'prettier/prettier': 'warn',
   },
   overrides: [
     {
       files: [
         '**/__tests__/*.{j,t}s?(x)',
-        '**/tests/unit/**/*.spec.{j,t}s?(x)'
+        '**/tests/unit/**/*.spec.{j,t}s?(x)',
       ],
       env: {
-        jest: true
-      }
-    }
-  ]
+        jest: true,
+      },
+    },
+  ],
 }
