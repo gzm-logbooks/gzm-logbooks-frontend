ba1ae82488b9a6a9418b1358a8aa373e3cacdeea
Jack Fletcher <jack.fletcher@sbgl.co.uk>
2020-12-18 17:42:34 +0000

    Add missing required validation plugin

--- src/services/DatabaseService.js
+++ src/services/DatabaseService.js
@@ -1,8 +1,12 @@
 import { createRxDatabase, addRxPlugin } from 'rxdb/plugins/core'
 import { RxDBDevModePlugin } from 'rxdb/plugins/dev-mode'
+import { RxDBValidatePlugin } from 'rxdb/plugins/validate';
 import * as IndexeddbAdaptor from 'pouchdb-adapter-indexeddb'
 
 import subjectSchema from '@/schemas/subject.json'
+
+// Add required plugins.
+addRxPlugin(RxDBValidatePlugin)
 
 // Add the dev-mode plugin.
 if (process.env.NODE_ENV === 'development') {
