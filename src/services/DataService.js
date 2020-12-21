ff96c3165c73d65baf60369dcae3b84f33d1a676
Jack Fletcher <jack.fletcher@sbgl.co.uk>
2020-12-21 00:31:34 +0000

    Update schemas

--- src/services/DataService.js
+++ src/services/DataService.js
@@ -1,15 +1,17 @@
 import { createRxDatabase, addRxPlugin } from 'rxdb/plugins/core'
 import { RxDBDevModePlugin } from 'rxdb/plugins/dev-mode'
+import { RxDBQueryBuilderPlugin } from 'rxdb/plugins/query-builder'
 import { RxDBValidatePlugin } from 'rxdb/plugins/validate'
 import * as IndexeddbAdaptor from 'pouchdb-adapter-indexeddb'
 
 import subjectSchema from '@/schemas/subject.json'
 import entrySchema from '@/schemas/entry.json'
 
-// Add required plugins.
+// Add plugins.
 addRxPlugin(RxDBValidatePlugin)
+addRxPlugin(RxDBQueryBuilderPlugin)
 
-// Add the dev-mode plugin.
+// Add the dev plugins.
 if (process.env.NODE_ENV === 'development') {
   addRxPlugin(RxDBDevModePlugin)
 }
