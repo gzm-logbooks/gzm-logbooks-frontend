3fceadcffd17df22620776284abacc5832fb0267
Jack Fletcher <jackowenfletcher@gmail.com>
2021-03-22 21:47:18 +0000

    Fresh Nuxt install

---
+++ pages/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="container">
+    <div>
+      <Logo />
+      <h1 class="title">growth-app</h1>
+      <div class="links">
+        <a
+          href="https://nuxtjs.org/"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="button--green"
+        >
+          Documentation
+        </a>
+        <a
+          href="https://github.com/nuxt/nuxt.js"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="button--grey"
+        >
+          GitHub
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
