6382b58d991b1a8b61eeb597ebdec3cad92951e6
Jack Fletcher <jack.fletcher@sbgl.co.uk>
2021-03-25 22:55:37 +0000

    Fancy indigo boxes

--- components/Header.vue
+++ components/Header.vue
@@ -1,9 +1,11 @@
 <template>
-  <div class="dark:bg-gray-800 text-gray-200">
-    <div class="max-w-xl mx-auto px-4 sm:px-6 h-16">
+  <div
+    class="bg-indigo-200 border-b border-indigo-800 text-indigo-800 shadow-inner"
+  >
+    <div class="max-w-xl mx-auto px-4 py-4 sm:px-6 h-16">
       <div class="flex items-center md:justify-start">
         <div class="flex justify-start lg:w-0 lg:flex-1">
-          <a href="#" class="xl:font-bold"> growth-app </a>
+          <a href="#" class="font-bold"> growth-app </a>
         </div>
       </div>
     </div>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,11 @@
 <template>
-  <div class="bg-gray-200">
+  <div class="bg-gray-50 flex-grow">
     <Header />
 
-    <div class="max-w-xl">
-      <Nuxt />
+    <div class="sm:max-w-xl mx-auto sm:my-6">
+      <div class="p-6 bg-white sm:rounded border border-indigo-200 shadow-sm">
+        <Nuxt />
+      </div>
     </div>
   </div>
 </template>
@@ -13,6 +15,20 @@ export default {
   mounted() {
     // Print routes for debug.
     console.log('Routes...', this.$nuxt.context.app.router.getRoutes())
-  }
+  },
 }
 </script>
+
+<style>
+html,
+body,
+#__nuxt,
+#__layout {
+  height: 100%;
+}
+
+#__layout {
+  display: flex;
+  flex-direction: column;
+}
+</style>
