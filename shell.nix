cfe9441f8e4bdd2698e1636f54761a6b7c0edf74
Jack Fletcher <jackowenfletcher@gmail.com>
2022-03-24 21:50:09 +0000

    Update shell.nix

--- shell.nix
+++ shell.nix
@@ -2,8 +2,12 @@
 
 pkgs.mkShell {
   buildInputs = [
+    # JS lang and dev tools
     pkgs.nodejs-17_x
     pkgs.yarn
+
+    # Deploy tools
+    # pkgs.netlify-cli (broken)
 
     # keep this line if you use bash
     # pkgs.bashInteractive
