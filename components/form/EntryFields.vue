fe3bf3c6f2a13fa35e5c1c4a5043d7532e4a2637
Jack Fletcher <jackowenfletcher@gmail.com>
2022-04-09 11:00:38 +0100

    Make logbook entry fields single column

--- components/form/EntryFields.vue
+++ components/form/EntryFields.vue
@@ -1,31 +1,28 @@
 <template>
-  <div class="grid-cols-3 gap-4 sm:grid">
-    <div class="col-span-2">
-      <FormulateInput
-        type="mood-circle"
-        name="mood"
-        label="Mood"
-        validation="required"
-        label-class="hidden"
-      />
-    </div>
+  <div class="">
+    <FormulateInput
+      type="mood-circle"
+      name="mood"
+      label="Mood"
+      validation="required"
+      label-class="hidden"
+    />
 
-    <div class="flex flex-col space-around">
-      <FormulateInput
-        type="date"
-        name="timestamp"
-        label="Date"
-        validation="required|after:2000-01-01"
-        error-behavior="live"
-        input-class="w-full input"
-      />
+    <FormulateInput
+      type="date"
+      name="timestamp"
+      label="Date"
+      validation="required|after:2000-01-01"
+      error-behavior="live"
+      class="w-full"
+    />
 
-      <FormulateInput
-        input-class="w-full input"
-        type="textarea"
-        name="comment"
-        label="Comment"
-      />
-    </div>
+    <!-- TODO: Fix width. -->
+    <FormulateInput
+      class="w-full"
+      type="textarea"
+      name="comment"
+      label="Comment"
+    />
   </div>
 </template>
---
+++ components/form/EntryFieldsTwoCol.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="grid-cols-3 gap-4 sm:grid">
+    <div class="col-span-2">
+      <FormulateInput
+        type="mood-circle"
+        name="mood"
+        label="Mood"
+        validation="required"
+        label-class="hidden"
+      />
+    </div>
+
+    <div class="flex flex-col space-around">
+      <FormulateInput
+        type="date"
+        name="timestamp"
+        label="Date"
+        validation="required|after:2000-01-01"
+        error-behavior="live"
+        input-class="w-full"
+      />
+
+      <FormulateInput
+        input-class="w-full"
+        type="textarea"
+        name="comment"
+        label="Comment"
+      />
+    </div>
+  </div>
+</template>
