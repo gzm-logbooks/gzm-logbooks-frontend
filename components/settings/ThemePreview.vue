9952f8fe098da7160bee023f2d92e61d338cd909
KatiePup <katie.c.a.baker@gmail.com>
2022-04-09 18:54:12 +0100

    Move settings theme preview to seperate file

---
+++ components/settings/ThemePreview.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <svg width="100" viewBox="0 0 20 24" xmlns="http://www.w3.org/2000/svg">
+      <rect x="0" y="0" width="20" height="24" rx="2" ry="2" fill="gray" />
+      <circle cx="10" cy="10" r="9" fill="red" />
+      <circle cx="10" cy="10" r="6" fill="orange" />
+      <circle cx="10" cy="10" r="3" fill="green" />
+      <rect x="1" y="20" width="18" height="3" rx="2" ry="2" fill="black" />
+    </svg>
+  </div>
+</template>
--- pages/settings.vue
+++ pages/settings.vue
@@ -23,36 +23,9 @@
       <template #title>
         <h2>Theme</h2>
       </template>
+
       <div class="flex space-x-3">
-        <div v-for="theme in themes" :key="theme.key">
-          <svg
-            width="100"
-            viewBox="0 0 20 24"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <rect
-              x="0"
-              y="0"
-              width="20"
-              height="24"
-              rx="2"
-              ry="2"
-              fill="gray"
-            />
-            <circle cx="10" cy="10" r="9" fill="red" />
-            <circle cx="10" cy="10" r="6" fill="orange" />
-            <circle cx="10" cy="10" r="3" fill="green" />
-            <rect
-              x="1"
-              y="20"
-              width="18"
-              height="3"
-              rx="2"
-              ry="2"
-              fill="black"
-            />
-          </svg>
-        </div>
+        <SettingsThemePreview v-for="theme in themes" :key="theme.key" />
       </div>
     </Card>
   </LayoutPage>
