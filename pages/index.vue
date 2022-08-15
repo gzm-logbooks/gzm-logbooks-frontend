49a2761d6ad59014ea9745c72a45b7ddfe46fc45
Jack Fletcher <jackowenfletcher@gmail.com>
2022-08-15 23:06:34 +0100

    Fix entry redirect

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,8 @@
-<script lang="ts">
-export default {
-  middleware({ redirect, app }) {
-    redirect({ name: 'logbooks' })
-  },
-}
+<script setup lang="ts">
+  await navigateTo('/logbooks')
+// definePageMeta({
+//   middleware: function() {
+//     console.log(123123)
+//   },
+// })
 </script>
