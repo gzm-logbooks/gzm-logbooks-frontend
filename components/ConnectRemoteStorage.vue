8c4475b73f4e3cee7d64a1c9e727e9bbceaa7253
Jack Fletcher <jackowenfletcher@gmail.com>
2024-02-05 22:52:18 +0000

    Fix remote storage widget

--- components/ConnectRemoteStorage.vue
+++ components/ConnectRemoteStorage.vue
@@ -2,29 +2,32 @@
   <div id="remoteStorageWidget" ref="widgetContainer" />
 </template>
 
-<script lang="ts">
+<script setup lang="ts">
 import Widget from 'remotestorage-widget'
 
-export default {
-  async mounted () {
-    const { $remoteStorage: storage } = this
-    const { widgetContainer: element } = this.$refs
+const widgetContainer = ref()
 
-    console.log({ storage, element })
+onMounted (async () => {
+  const { $remoteStorage: storage } = useNuxtApp()
 
-    if (!storage || !element) {
-      throw new Error('Could not set up remote storage widget.')
-    }
+  const element = widgetContainer.value
 
-    //
-    await storage.on('ready', async () => {
-      console.log({ Widget })
-      // const Widget = await import('remotestorage-widget')
-      const widget = Widget
-      // .then(
-      //   (widget) => widget.attach(element.id)
-      // )
-    })
+  console.log({ storage, element })
+
+  if (!storage || !element) {
+    throw new Error('Could not set up remote storage widget.')
   }
-}
+
+  //
+  await storage.on('ready', async () => {
+    console.log({ Widget })
+    // const Widget = await import('remotestorage-widget')
+    // .then(
+    //   (widget) => widget.attach(element.id)
+    // )
+
+    const widget = new Widget(storage)
+    widget.attach(element.id)
+  })
+})
 </script>
